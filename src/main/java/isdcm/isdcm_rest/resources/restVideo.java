5c298c80ba5315e6d9537368e9517e4ef4db7534
Pavel Khralovich <pkhralovich@gmail.com>
2021-04-22 17:57:59 +0200

    Refact error response

--- src/main/java/isdcm/isdcm_rest/resources/restVideo.java
+++ src/main/java/isdcm/isdcm_rest/resources/restVideo.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package isdcm.isdcm_rest.resources;
 
 import isdcm.models.Video;
 import java.sql.SQLException;
-
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Path;
@@ -17,30 +9,43 @@ import javax.ws.rs.PUT;
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.core.MediaType;
 import javax.json.JsonObject;
-import javax.json.Json;
 import javax.ws.rs.core.Response;
 
 @Path("video")
 @RequestScoped
 public class restVideo {
-
-    @Context
-    private UriInfo context;
-
-    public restVideo() {
-    }
-
+    private static final String PARAM_ID_VIDEO = "id_video";
+    private static final String PARAM_ID_USER = "id_user";
+    
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response putJson(JsonObject content){
+    public Response put(JsonObject content){
         try {
-            boolean oRes = Video.updateReproductions(content.getInt("id_video"), content.getInt("id_user"));
-            if (oRes) return Response.ok(Video.getVideoById(content.getInt("id_video")), MediaType.APPLICATION_JSON).build();
-            else return Response.serverError().entity("{ \"error\": \"Video not found\" }").build();
-        } catch (SQLException handlerException) {
-            return Response.serverError().entity("{ \"error\": \"Video not found\" }").build();
+            int idVideo = content.getInt(PARAM_ID_VIDEO);
+            int idUser = content.getInt(PARAM_ID_USER);
+            
+            boolean updated = Video.updateReproductions(idVideo, idUser);
+            if (updated) return Response.ok(Video.getVideoById(idVideo), MediaType.APPLICATION_JSON).build();
+            else return Response.serverError().entity(new Error("Video not found")).build();
+        } catch (SQLException e) {
+            return Response.serverError().entity(new Error("Video not found: " + e.getMessage())).build();
         }
     }
     
+    public class Error {
+        private String message;
+        
+        Error(String error) {
+            this.message = error;
+        }
+        
+        public String getMessage() {
+            return message;
+        }
+        
+        public void setMessage(String error) {
+            this.message = error;
+        }
+    }
 }
